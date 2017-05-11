7aac48c867a0ff9dd3692905304d5e12479f762b
Florence <allureteartop@163.com>
2017-05-11 10:56:16 +0800

    1.项目初始化

---
+++ FZCategory/FZCategory/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FZCategory
+//
+//  Created by Florence on 2017/5/11.
+//  Copyright © 2017年 AllureTeartop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
